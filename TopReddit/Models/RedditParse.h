bd016fb69ec8608a748b8cd9e022073d6c41fb59
Tracey Eubanks <traceyeubanks@yahoo.com>
2013-11-13 15:21:22 -0700

    Added login method to RedditParse
    Login Method, delegate protocol for when we successfully login and when
    we don't successfully login, and different constants to handle logging
    in

--- TopReddit/Models/RedditParse.h
+++ TopReddit/Models/RedditParse.h
@@ -8,6 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+extern const NSString *kRedditAPIURL;
+extern NSString *kRedditUsername;
+extern NSString *kRedditPassword;
+
+@protocol RedditParseLoginProtocol <NSObject>
+
+@required
+-(void)didLogin;
+-(void)didReceiveError:(NSDictionary*)errorInfo;
+
+@end
+
 @interface RedditParse : NSObject
+@property (strong, nonatomic) NSString *modhash; // reddit header for authentication
+@property (strong, nonatomic) NSOperationQueue *apiRequestQueue;
+@property (weak, nonatomic) id delegate;
+
+-(UIImage*)firstImage;
+-(void)upVote;
+-(void)downVote;
+
+-(void)loginWithUsername:(NSString*)username andPassword:(NSString*)password;
 
 @end
--- TopReddit/Models/RedditParse.m
+++ TopReddit/Models/RedditParse.m
@@ -7,8 +7,54 @@
 //
 
 #import "RedditParse.h"
-NSString *API_URL = @"http://api.reddit.com/r/funny";
+
+const NSString *kRedditAPIURL = @"http://api.reddit.com";
+NSString *kRedditUsername = @"youmakemydaybrighter";
+NSString *kRedditPassword = @"singularity";
 
 @implementation RedditParse
+-(id)init{
+    self = [super init];
+    if(self){
+        _apiRequestQueue = [[NSOperationQueue alloc] init];
+        [_apiRequestQueue setMaxConcurrentOperationCount:3];
 
+    }
+    return self;
+}
+
+-(void)loginWithUsername:(NSString *)username andPassword:(NSString *)password {
+    NSString *loginBodyString = [NSString stringWithFormat:@"api_type=json&rem=false&username=%@&passwd=%@", username, password];
+    NSData *loginData = [loginBodyString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSString *loginURLString = [NSString stringWithFormat:@"%@/api/login", kRedditAPIURL];
+    NSURL *loginURL = [NSURL URLWithString:loginURLString];
+    NSMutableURLRequest *mutableLoginRequest = [[NSMutableURLRequest alloc] initWithURL:loginURL];
+    [mutableLoginRequest setHTTPMethod:@"POST"];
+    [mutableLoginRequest setHTTPBody:loginData];
+    
+    [NSURLConnection sendAsynchronousRequest:mutableLoginRequest queue:_apiRequestQueue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        if(connectionError)
+            [self.delegate didReceiveError:[connectionError userInfo]];
+
+        NSError *error = nil;
+        NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        responseDictionary = [responseDictionary objectForKey:@"json"]; // remove json key, don't need it
+        if(error)
+            [self.delegate didReceiveError:[error userInfo]];
+
+        NSArray *loginErrors = [[responseDictionary objectForKey:@"errors"] objectAtIndex:0];
+        if([loginErrors count] > 0) {
+            NSMutableDictionary *errorInfo = [[NSMutableDictionary alloc] init];
+            [errorInfo setValue:[loginErrors objectAtIndex:0] forKey:@"errorConstant"];
+            [errorInfo setValue:[loginErrors objectAtIndex:1] forKey:@"errorMessage"];
+            [errorInfo setValue:[loginErrors objectAtIndex:2] forKey:@"eckifino"];
+            [self.delegate didReceiveError:errorInfo];
+        }
+    }];
+}
+
+-(UIImage *)firstImage {
+
+}
 @end
