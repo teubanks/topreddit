26b72b685b85325cf783093afb54832553ce911a
Tracey Eubanks <traceyeubanks@yahoo.com>
2013-11-13 13:30:17 -0700

    Initial Commit

---
+++ TopReddit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TopReddit
+//
+//  Created by Tracey Eubanks on 11/13/13.
+//  Copyright (c) 2013 Tracey Eubanks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopReddit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TopReddit
+//
+//  Created by Tracey Eubanks on 11/13/13.
+//  Copyright (c) 2013 Tracey Eubanks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
