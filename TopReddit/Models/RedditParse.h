592be8b91ba64971fe2ff79bde1317d54e6951aa
Tracey Eubanks <traceyeubanks@yahoo.com>
2013-11-13 13:39:47 -0700

    initial layout for iPhone

---
+++ TopReddit/Models/RedditParse.h
@@ -0,0 +1,13 @@
+//
+//  RedditParse.h
+//  TopReddit
+//
+//  Created by Tracey Eubanks on 11/13/13.
+//  Copyright (c) 2013 Tracey Eubanks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RedditParse : NSObject
+
+@end
---
+++ TopReddit/Models/RedditParse.m
@@ -0,0 +1,14 @@
+//
+//  RedditParse.m
+//  TopReddit
+//
+//  Created by Tracey Eubanks on 11/13/13.
+//  Copyright (c) 2013 Tracey Eubanks. All rights reserved.
+//
+
+#import "RedditParse.h"
+NSString *API_URL = @"http://api.reddit.com/r/funny";
+
+@implementation RedditParse
+
+@end
